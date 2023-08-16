9831325eac716e18b66e6d503b9c01e5918d99f5
Maged Faiz <magedfaiz98@gmail.com>
2023-08-16 17:53:55 +0200

    feat: initialized a new Pages Router next

--- apps/agent/tailwind.config.ts
+++ apps/agent/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
@@ -15,9 +15,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: ["lofi"],
-  },
-};
-export default config;
+  plugins: [],
+}
+export default config
