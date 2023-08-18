273731bf08c03c19dc61e428f773386111f44d53
Eman <emmanuelgatwech@gmail.com>
2023-08-18 17:27:07 +0300

    feat: add business registration form

--- apps/agent/src/componens/clients/InsertNewClient.tsx
+++ apps/agent/src/componens/clients/InsertNewClient.tsx
@@ -1,9 +1,26 @@
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  businessName: string;
+};
 
 export const InsertNewClient = () => {
-    return (
-        <div>
-            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        </div>
-    )
-}
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Type here"
+        className="input input-bordered w-full max-w-xs"
+        defaultValue="test"
+        {...register("businessName")}
+      />
+    </form>
+  );
+};
