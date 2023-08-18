15ac86953abb5351c4b20f6a2558c0d8d5bc58cc
Eman <emmanuelgatwech@gmail.com>
2023-08-18 17:20:59 +0300

    chore: draft PR

---
+++ apps/agent/src/componens/clients/InsertNewClient.tsx
@@ -0,0 +1,9 @@
+import { useForm, SubmitHandler } from "react-hook-form"
+
+export const InsertNewClient = () => {
+    return (
+        <div>
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+        </div>
+    )
+}
