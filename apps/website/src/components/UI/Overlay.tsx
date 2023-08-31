5ebc911410a9bd6ce2d2213020838470b3fbe78d
Mabior Duom <mabiorduom012@gmail.com>
2023-08-31 15:50:38 +0300

    feat: added a responsive navbar

---
+++ apps/website/src/components/UI/Overlay.tsx
@@ -0,0 +1,8 @@
+
+const Overlay = () => {
+  return (
+    <div className='absolute top-0 left-0 w-full h-[1000%] bg-[rgba(0,0,0,0.5)] transition-[0.3s] duration-[ease] z-[100]'></div>
+  )
+}
+
+export default Overlay
