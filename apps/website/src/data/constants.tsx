fce1eb8ce485acdc619f52d459b4341ba7cf656e
Mabior Duom <mabiorduom012@gmail.com>
2023-10-15 11:37:00 +0300

    feat: added faqs & footer section

--- apps/website/src/data/constants.tsx
+++ apps/website/src/data/constants.tsx
@@ -20,6 +20,12 @@ interface FeaturesData {
     desc: string;
 }
 
+interface FaqData {
+    id: number;
+    title: string;
+    desc: string;
+}
+
 export const servicesData: ServiceData[] = [
     {
         id: 1,
@@ -65,5 +71,33 @@ export const featuresData: FeaturesData[] = [
         image: featureThree,
         title: 'Delivery Process',
         desc: 'Flexible delivery. Choose from community pick-up points or input specific addresses. Includes confirmation, coordination, validation, and delivery time selection.',
+    },
+];
+
+export const faqData: FaqData[] = [
+    {
+        id: 1,
+        title: 'How can I register as a business or supplier?',
+        desc: "You can register by creating an account on our platform. During registration, you'll be asked to provide necessary information about your business or the products/services you offer.",
+    },
+    {
+        id: 2,
+        title: 'What type of businesses and suppliers can register?',
+        desc: "Our platform caters to a wide range of businesses and suppliers. Whether you're a startup, a small business, or a large corporation, you're welcome to register.",
+    },
+    {
+        id: 3,
+        title: 'How does the application simplify procurement processes?',
+        desc: 'We streamline procurement by providing a platform where businesses can easily find and connect with suppliers that match their needs.',
+    },
+    {
+        id: 4,
+        title: 'What delivery services are offered?',
+        desc: 'We facilitate the delivery of products from suppliers to businesses, and from businesses to customers. The specifics of these services can vary based on the supplier and the type of product.',
+    },
+    {
+        id: 5,
+        title: 'How does the application connect with the market?',
+        desc: 'Our platform serves as a marketplace where businesses, suppliers, and customers can interact, exchange information, and conduct transactions.',
     },
 ];
