966407a2655df7e3e008fe6df7c720489b77c121
Maged Faiz <magedfaiz98@gmail.com>
2023-10-25 15:36:18 +0200

    feat: enhanced suppliers card

--- apps/agent/src/components/Suppliers/ListSuppliers.tsx
+++ apps/agent/src/components/Suppliers/ListSuppliers.tsx
@@ -1,12 +1,16 @@
-import { SupplierOverviewCard } from "./SupplierOverviewCard";
-import { useFetchSuppliers } from "@/hooks/suppliers";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { SupplierOverviewCard } from './SupplierOverviewCard';
+import { useFetchSuppliers } from '@/hooks/suppliers';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 export type SahilSupplier = {
-  name: string;
   id: string;
+  name: string;
+  phoneNumber: string;
+  streetAddress: string;
+  categories: Array<{
+    category_name: string;
+  }>;
 };
-
 
 export const ListSuppliers = () => {
   const { data: suppliers, error, loading } = useFetchSuppliers();
@@ -15,14 +19,17 @@ export const ListSuppliers = () => {
 
   if (error) {
     return (
-      <div className="card card-compact bg-white shadow">
-        <div className="card-body">
-        <h3 className="card-title">Something went wrong...</h3>
-        <p>Error loading suppliers, check your network connection. You can also try refreshing the page.</p>
-        <div className="card-actions">
-          <button className="btn btn-sm btn-secondary">Reload</button>
-          <button className="btn btn-sm btn-outline">Go Back</button>
-        </div>
+      <div className='card card-compact bg-white shadow'>
+        <div className='card-body'>
+          <h3 className='card-title'>Something went wrong...</h3>
+          <p>
+            Error loading suppliers, check your network connection. You can also
+            try refreshing the page.
+          </p>
+          <div className='card-actions'>
+            <button className='btn btn-sm btn-secondary'>Reload</button>
+            <button className='btn btn-sm btn-outline'>Go Back</button>
+          </div>
         </div>
       </div>
     );
@@ -36,11 +43,13 @@ export const ListSuppliers = () => {
     );
   }
 
+  console.log(suppliers);
   return (
-    <div className="flex gap-2" ref={parent}>
-      {suppliers && suppliers.map((supplier: SahilSupplier) => (
-        <SupplierOverviewCard key={supplier.id} supplier={supplier} />
-      ))}
+    <div className='flex flex-col gap-2' ref={parent}>
+      {suppliers &&
+        suppliers.map((supplier: SahilSupplier) => (
+          <SupplierOverviewCard key={supplier.id} supplier={supplier} />
+        ))}
     </div>
   );
 };
--- apps/agent/src/lib/graphql/queries/suppliers.ts
+++ apps/agent/src/lib/graphql/queries/suppliers.ts
@@ -1,19 +1,31 @@
-import {
-    gql,
-} from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const FETCH_SUPPLIERS = gql`
-query getSuppliers {
-    suppliers {
-        created_at
-        id
-        name
+  query getSuppliers(
+    $category_name: supplier_categories_enum_enum_comparison_exp = {}
+    $offset: Int = 0
+    $order_by: [suppliers_order_by!] = {}
+    $limit: Int = 10
+  ) {
+    suppliers(
+      where: { categories: { category_name: $category_name } }
+      offset: $offset
+      order_by: $order_by
+      limit: $limit
+    ) {
+      id
+      name
+      streetAddress
+      phoneNumber
+      categories {
+        category_name
+      }
     }
-}
+  }
 `;
 
 export const FETCH_SUPPLIER_BY_PK = gql`
-query getSupplierByPK($id: uuid!) {
+  query getSupplierByPK($id: uuid!) {
     suppliers_by_pk(id: $id) {
       created_at
       id
@@ -23,5 +35,5 @@ query getSupplierByPK($id: uuid!) {
       contactName
       contactEmail
     }
-}
+  }
 `;
