36fe4a456dcd0b7fa501798724bf8ef02ab323ab
Eman <emmanuelgatwech@gmail.com>
2023-11-04 20:37:57 +0300

    feat: setup express

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -2,11 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/Layout/layout";
 import { ApolloProvider } from "@apollo/client";
+import { client } from "@/lib/graphql/apolloClient";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout><Component {...pageProps} /></Layout>
+    </ApolloProvider>
   );
 }
