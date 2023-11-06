72625638fb27e3a33564ab27810cf818ecccf388
Maged Faiz <magedfaiz98@gmail.com>
2023-11-06 22:54:58 +0200

    feat: improved courier query

--- apps/agent/src/lib/graphql/queries/couriers.ts
+++ apps/agent/src/lib/graphql/queries/couriers.ts
@@ -1,29 +1,30 @@
-import {
-    gql,
-} from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const FETCH_COURIERS = gql`
-query getCouriers {
+  query getCouriers {
     couriers {
-        created_at
-        id
-        avatar
-        name
+      id
+      avatar
+      name
+      DOB
+      phoneNumber
+      gender
+      email
     }
     couriers_aggregate {
       aggregate {
         count(columns: id, distinct: true)
       }
     }
-}
+  }
 `;
 
 export const FETCH_COURIER_BY_PK = gql`
-query getCouriersByPK($id: uuid!) {
+  query getCouriersByPK($id: uuid!) {
     couriers_by_pk(id: $id) {
-        id
-        name
-        avatar
+      id
+      name
+      avatar
     }
-}
+  }
 `;
