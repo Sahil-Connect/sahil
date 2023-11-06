48da170ddc8c2cbf5dbe1853df43af23bfc8f086
Maged Faiz <magedfaiz98@gmail.com>
2023-11-06 22:15:15 +0200

    feat: added Courier Summary Form
    - Added insert_courier mutation
    - Added useRegisterCourier Hook

---
+++ apps/agent/src/lib/graphql/mutations/couriers.ts
@@ -0,0 +1,10 @@
+import { gql } from '@apollo/client';
+
+export const INSERT_NEW_COURIER = gql`
+  mutation insertNewCourier($object: couriers_insert_input = {}) {
+    insert_couriers_one(object: $object) {
+      id
+      name
+    }
+  }
+`;
