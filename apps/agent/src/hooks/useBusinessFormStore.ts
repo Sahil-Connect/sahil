a632ca178986b59125a3e022ebee2d0dfc3e1ec8
Maged Faiz <magedfaiz98@gmail.com>
2023-11-06 19:32:11 +0200

    feat: refactored useBusinessFormStore

--- apps/agent/src/hooks/useBusinessFormStore.ts
+++ apps/agent/src/hooks/useBusinessFormStore.ts
@@ -1,59 +1,12 @@
-import { create } from 'zustand';
-import { INITIAL_STEP, steps } from '@/Orders/constants';
-export type StepDirection = "prev" | "next";
+import { createFormStore } from '@sahil/lib';
 
-export type SahilBusinessPartner = Record<string, any>;
+const INITIAL_STEP = 'business_info';
 
-type OrderFormStore = {
-    currentStep: typeof steps[number];
-    steps: typeof steps;
-    goToStep: (direction: StepDirection) => void;
-    updateStepByIndex: (stepIndex: number) => void;
-    updateStepFormData: (formData: Record<string, any>) => void;
-    formData: Record<string, any>;
-}
+const steps = [
+  'business_info',
+  'address_info',
+  'preferences',
+  'summary',
+] as const;
 
-export const useBusinessFormStore = create<OrderFormStore>((set) => ({
-    currentStep: INITIAL_STEP,
-    formData: {},
-    steps,
-    goToStep: (direction: StepDirection) => {
-        set((state) => {
-            const currentIndex = steps.indexOf(state.currentStep);
-            if (currentIndex !== -1 && currentIndex <= steps.length - 1) {
-                return ({
-                    ...state,
-                    currentStep: direction === "prev" ? steps[currentIndex - 1] : steps[currentIndex + 1]
-                });
-            }
-            return ({
-                ...state
-            });
-        });
-    },
-    updateStepByIndex: (stepIndex: number) => {
-        set((state) => {
-            const currentIndex = steps.indexOf(state.currentStep);
-            if (currentIndex !== -1 && currentIndex <= steps.length - 1) {
-                return ({
-                    ...state,
-                    currentStep: steps[stepIndex]
-                });
-            }
-            return ({
-                ...state
-            });
-        });
-    },
-    updateStepFormData: (formData: Record<string, any>) => {
-        set((state) => {
-            return ({
-                ...state,
-                formData: {
-                    ...state.formData,
-                    ...formData,
-                }
-            })
-        });
-    }
-}));
+export const useBusinessFormStore = createFormStore(INITIAL_STEP, steps);
