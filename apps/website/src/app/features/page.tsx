c7ae6e8df661ec24d278eee8a79289f10fbc3dad
Mabior Duom <mabiorduom012@gmail.com>
2023-12-28 20:57:28 +0300

    feat: added pages

---
+++ apps/website/src/app/features/page.tsx
@@ -0,0 +1,25 @@
+import { 
+  PageTitle, 
+  GridContainer, 
+  ComingSoon,
+} from '@/components/shared'
+
+export const metadata = {
+  title: 'Features - Sahil App',
+  description: 'Discover the wide range of services we offer. Our innovative solutions are designed to help you streamline your business operations.',
+}
+
+export default function FeaturesPage() {
+  return (
+    <>
+      <PageTitle 
+        title="Features" 
+        subtitle="Our Services" 
+        description="Explore the wide range of features we offer to help you revolutionize your business operations."
+      />
+      <GridContainer>
+        <ComingSoon />
+      </GridContainer>
+    </>
+  )
+}
