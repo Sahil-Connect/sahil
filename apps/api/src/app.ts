8db10d8eaf62e502cc61a99b38625ef8ab88b886
Eman <emmanuelgatwech@gmail.com>
2023-12-29 18:44:40 +0300

    chore init backend project

---
+++ apps/api/src/app.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+const express = require("express");
+export const app = express();
+import { databaseURL } from "./config";
+import { init } from "./init";
+import connect from "./db";
+
+dotenv.config();
+const connection = connect(databaseURL);
+
+/** initialize middleware */
+init(app);
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Express + TypeScript Server");
+// });
---
+++ apps/api/src/routers/index.ts
@@ -0,0 +1,9 @@
+import * as express from "express";
+
+import api from "./api";
+
+const router = express.Router();
+
+router.use("/api", api);
+
+export default router;
