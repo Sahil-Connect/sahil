3e4e9eb3ce09c1736c9510c99dec8571d3a67a61
Eman <emmanuelgatwech@gmail.com>
2024-01-25 18:24:27 +0300

    feat: validate business registration

--- apps/api/src/businesses/operations/registerBusiness.ts
+++ apps/api/src/businesses/operations/registerBusiness.ts
@@ -9,17 +9,12 @@ type DeliveryMethod = "pick-up-location" | "direct-delivery";
 
 export const businessSchema = z.object({
   name: z.string().trim(),
-  //   type: z.string(),
-  //   description: z.string(),
-  //   contactEmail: z.string(),
-  contactName: z.string(),
-  //   preferredContactMethod: z.string(),
-  //   preferredDeliveryMethod: z.string()
-  //   price: z
-  //   .string()
-  //   .min(1, { message: "required" })
-  //   .transform((value) => +value)
-  //   .refine((value) => !isNaN(value), { message: "Must be a number"})
+  type: z.string(),
+  description: z.string().optional(),
+  contactEmail: z.string().optional(),
+  contactName: z.string().optional(),
+  preferredContactMethod: z.string().optional(),
+  preferredDeliveryMethod: z.string().optional(),
 });
 
 export type BusinessAttributes = z.infer<typeof businessSchema>;
