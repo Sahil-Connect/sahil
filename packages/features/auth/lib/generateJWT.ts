748e8ba126a58fd241a08b16d5055e8f8f2cb676
Eman <emmanuelgatwech@gmail.com>
2024-02-20 10:13:00 +0200

    feat: add jwt basic claims

---
+++ packages/features/auth/lib/generateJWT.ts
@@ -0,0 +1,35 @@
+import { HMAC } from "oslo/crypto";
+import { createJWT, validateJWT, parseJWT } from "oslo/jwt";
+import { TimeSpan } from "oslo";
+
+export const generateJWTClaim = (token) => {
+  const claims = {
+    sub: token?.sub.toString(),
+    name: token?.name,
+    email: token?.email,
+    iat: Date.now() / 1000,
+    exp: new TimeSpan(30, "d"),
+    "https://hasura.io/jwt/claims": {
+      "x-hasura-allowed-roles": ["user"],
+      "x-hasura-default-role": "user",
+      "x-hasura-role": "user",
+      "x-hasura-user-id": token.id,
+    },
+  };
+  return claims;
+};
+
+// const secret = await new HMAC("SHA-256").generateKey();
+
+export const generateJWT = async (payload, { secret }) => {
+  const jwt = await createJWT("HS256", secret, payload, {
+    expiresIn: new TimeSpan(30, "d"),
+    includeIssuedTimestamp: true,
+    ...payload,
+  });
+  return jwt;
+};
+
+export const decodeJWT = (token, { secret }) => {
+    return jwt.verify(token, secret, { algorithms: ["HS256"] });
+}
---
+++ packages/lib/auth/auth.ts
@@ -0,0 +1,112 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import FacebookProvider from "next-auth/providers/facebook";
+import GoogleProvider from "next-auth/providers/google";
+import { generateJWT, generateJWTClaim, decodeJWT } from "./generateJWT";
+
+const providers = [
+  // @ts-ignore
+  CredentialsProvider({
+    async authorize(credentials) {
+      console.log("we here!!");
+      console.log(credentials);
+      const authResponse = await fetch("http://localhost:8000/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      });
+
+      console.log(authResponse)
+
+      // if (!authResponse.ok) {
+      //   return null
+      // }
+
+      const user = await authResponse.json()
+
+      return user;
+    },
+  }),
+  FacebookProvider({
+    clientId: process.env.NEXT_PUBLIC_GITHUB_ID || "",
+    clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET || "",
+  }),
+  GoogleProvider({
+    clientId: process.env.NEXT_PUBLIC_GITHUB_ID || "",
+    clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET || "",
+  }),
+];
+
+const jwtConfig = (options: any) => {
+  return {
+    // A secret to use for key generation (you should set this explicitly)
+    secret: process.env.SECRET,
+    encode: async ({ secret, token, maxAge }: any) => {
+      const claims = generateJWTClaim(token);
+
+      const encodedToken = generateJWT(claims, { 
+        secret
+      });
+      return encodedToken;
+    },
+    decode: async ({ secret, token, maxAge }: any) => {
+      const decodedToken = decodeJWT(token, { secret })
+      return decodedToken;
+    },
+  };
+};
+
+const authPagesConfig = () => {
+  return {
+    signIn: "/auth/signin", // Displays signin buttons
+    signOut: "/auth/signout", // Displays form with sign out button
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // Used for check email page
+    newUser: "/auth/new", // If set, new users will be directed here on first sign in
+  };
+};
+
+const initNextAuth = () => {
+  return {
+    providers,
+    secret: process.env.SECRET,
+    // jwt: jwtConfig,
+    pages: authPagesConfig(),
+    callbacks: {
+      // @ts-ignore
+      async session({ session, token }) {
+        // @ts-ignore
+        const encodedToken = jwt.sign(token, process.env.SECRET, {
+          algorithm: "HS256",
+        });
+        // @ts-ignore
+        session.token = encodedToken;
+        // @ts-ignore
+        session.id = token.id;
+        return session;
+      },
+      // @ts-ignore
+      async jwt({ token, user, profile, account, isNewUser }) {
+        const isAuthenticated = user ? true : false;
+        if (isAuthenticated) {
+          token.id = user.id;
+        }
+        return Promise.resolve(token);
+      },
+      // @ts-ignore
+      async signIn({ user, account, profile, email, credentials }) {
+        return true;
+      },
+    },
+
+    // Events are useful for logging
+    // https://next-auth.js.org/configuration/events
+    events: {},
+
+    // Enable debug messages in the console if you are having problems
+    debug: true,
+  };
+};
+
+export default initNextAuth;
