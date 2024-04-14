856c6f00c652c349d1291c214f2d6339f21891e7
Eman <emmanuelgatwech@gmail.com>
2024-04-14 13:49:34 +0200

    feat: add types to matcher

--- apps/api/src/matcher/setGeofence.ts
+++ apps/api/src/matcher/setGeofence.ts
@@ -1,6 +1,6 @@
 import { client } from "../lib/graphql-request";
 
 
-export const setGeofence = async (lat, lng) => {
+export const setGeofence = async (lat: number, lng: number) => {
     // const coords = client.request();
 }
