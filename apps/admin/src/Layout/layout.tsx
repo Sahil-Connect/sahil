0827cddc3da8c75293c2cc947b730a3cebf102b5
mde3 <mabiorduom012@gmail.com>
2024-05-28 16:20:25 +0300

    feat: configure graphql

---
+++ apps/admin/src/Layout/layout.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from "react";
+import logo from "../../public/logo-alt.svg";
+import { useRouter } from "next/router";
+import { signOut, useSession } from "next-auth/react";
+import { Navbar } from "ui";
+type LayoutProps = {
+    children: ReactNode;
+};
+import {
+    HiOutlineUserGroup,
+    HiOutlineGlobeAlt,
+} from "react-icons/hi2";
+
+const links = [
+    {
+        name: "Users",
+        href: "/users",
+        icon: HiOutlineUserGroup,
+    },
+    {
+        name: "Zones",
+        href: "/zones",
+        icon: HiOutlineGlobeAlt,
+    },
+];
+
+export default function Layout({ children, ...props }: LayoutProps) {
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    const onSignOut = async () => {
+        await signOut();
+        router.push("/auth/signin");
+    };
+    return (
+        <>
+            <Navbar
+                links={links}
+                logo={logo}
+                header="Sahil Admin"
+                onSignOut={onSignOut}
+                user={session?.user}
+            />
+            <main className="p-4 bg-gray-50">
+                {children}
+            </main>
+        </>
+    );
+}
--- apps/admin/src/pages/_app.tsx
+++ apps/admin/src/pages/_app.tsx
@@ -1,6 +1,37 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import { SessionProvider } from "next-auth/react";
+import Layout from "@/Layout/layout";
+import { createApolloClient } from "@sahil/lib/graphql";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const graphqlUri = process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ENDPOINT as string;
+const ws = process.env.NEXT_PUBLIC_HASURA_GRAPHQL_WS as string;
+
+const httpOptions = {
+  headers: {
+    "x-hasura-admin-secret":
+      process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+  },
+};
+
+const client = createApolloClient({
+  uri: graphqlUri,
+  httpOptions,
+  ws: ws,
+});
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <ApolloProvider client={client}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </SessionProvider>
+  );
 }
