7421c253e150cd544114970b642ef079a7f96a6f
mde3 <mabiorduom012@gmail.com>
2024-05-28 16:53:31 +0300

    feat: add zones & users pages

---
+++ apps/admin/src/pages/zones/index.tsx
@@ -0,0 +1,12 @@
+
+export default function Zones() {
+  return (
+    <section className="space-y-2">
+      <div>
+        <h1>
+          Zones Page
+        </h1>
+      </div>
+    </section>
+  );
+}
