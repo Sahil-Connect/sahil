43cca5385816076a95fdbf283a8b64c61eacbd35
mde3 <mabiorduom012@gmail.com>
2024-05-28 16:11:55 +0300

    feat: add daisyui

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -14,7 +14,25 @@ const extendedConfig: Config = {
       ...baseConfig.theme?.extend,
     },
   },
-  plugins: [],
+  plugins: [...(baseConfig.plugins || []), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        lemonade: {
+          ...require("daisyui/src/theming/themes")["[data-theme=lemonade]"],
+          primary: "#067a46",
+          secondary: "#07ba3d",
+          accent: "#FFDB58",
+          neutral: "#2d2f39",
+          "bg-base-100": "#F2F2F3",
+          info: "#76d1e5",
+          success: "#22a05b",
+          warning: "#f0bc2d",
+          error: "#e33631",
+        },
+      },
+    ],
+  },
 };
 
 module.exports = extendedConfig;
