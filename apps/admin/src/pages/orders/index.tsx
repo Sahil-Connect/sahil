e7169d6581e5be5987d88ab7453b8f2ea03bbc3f
Eman <emmanuelgatwech@gmail.com>
2024-08-11 22:29:22 +0200

    feat: view orders in admin

--- apps/admin/src/pages/orders/index.tsx
+++ apps/admin/src/pages/orders/index.tsx
@@ -1,10 +1,32 @@
+import { useRouter } from "next/router";
+import { HiPlus, HiOutlineDocumentMagnifyingGlass } from "react-icons/hi2";
+import { ListOrders } from "@sahil/features/Orders/ListOrders";
+import { Card, Stats, Stat } from "ui";
+
 export default function Orders() {
-    return (
-      <section className="space-y-2">
-        <div>
-          <h1>Orders Page</h1>
+  const router = useRouter();
+  return (
+    <section className="space-y-4">
+      <Card>
+        <div className="flex justify-between items-center">
+          <div>
+            <h1 className="text-xl">Orders</h1>
+          </div>
+          <div className="flex gap-2">
+            <button className="btn btn-sm">
+              <HiOutlineDocumentMagnifyingGlass />
+              track Order
+            </button>
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={() => router.push("/orders/new/order_details")}
+            >
+              <HiPlus /> New Order
+            </button>
+          </div>
         </div>
-      </section>
-    );
-  }
-  
+      </Card>
+      <ListOrders />
+    </section>
+  );
+}
---
+++ packages/features/Zones/ZoneOverviewCard.tsx
@@ -0,0 +1,3 @@
+export const ZoneOverviewCard = () => {
+  return <div>ZoneOverviewCard</div>;
+};
